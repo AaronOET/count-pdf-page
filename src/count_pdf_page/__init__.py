ca75a35b2548cddff5296c50b5dc1f3da88f3db4
AaronOET <aaronhsu@mail.ntou.edu.tw>
2025-08-02 05:11:33 +0800

    create release 0.1.1

--- src/count_pdf_page/__init__.py
+++ src/count_pdf_page/__init__.py
@@ -5,8 +5,8 @@ This package provides utilities to count pages in PDF files and generate
 markdown reports with detailed statistics.
 """
 
-__version__ = "0.1.0"
-__author__ = "AaronOET"
+__version__ = "0.1.1"
+__author__ = "Chih-Hung Hsu"
 __email__ = "aaronhsu@mail.ntou.edu.tw"
 
 from .core import count_pdf_pages, generate_markdown_report
